1b5e498d6639792ab53316c1a44582f45294bcb2
ya.kashmanaft <ya.kashmanaft@yandex.ru>
2022-05-24 23:51:53 +0500

    исправлен баг отступов и блюр фона в тотал инфе при создании нового дела

--- src/views/CreateDeal.vue
+++ src/views/CreateDeal.vue
@@ -35,11 +35,11 @@
           <!-- Может имеет смысл сделать компонентом? -->
           <div
             v-if="dealsList.length !== 0"
-            class="w-full fixed bottom-0 left-0 flex items-center border-t justify-center flex-col pb-0 z-20"
+            class="w-full fixed bottom-0 left-0 flex items-center  justify-center flex-col pb-0 z-20"
             :class="{ totalMenu_wrapper:totalDealMenu }"
           >
             <!-- Total Menu -->
-            <div class="bg-light-grey w-full fixed bottom-0 rounded-t-3xl" :class="{ totalMenu:totalDealMenu }">
+            <div class="bg-light-grey border-t w-full fixed bottom-0 rounded-t-3xl" :class="{ totalMenu:totalDealMenu }">
 
               <!-- Header -->
               <div class="flex place-content-between px-4">
@@ -127,7 +127,7 @@
           </div>
 
           <!-- set deal information inputs -->
-          <div class="w-full" :class="{fixed:totalDealMenu}">
+          <div class="w-full mb-32" :class="{deal_information_inputs:totalDealMenu}">
 
             <!-- С кем заключается дело (Укажите контакт) -->
             <div class="w-full">
@@ -376,181 +376,11 @@
             <p>Оплачено: 1000,00 руб.</p>
             <p>Задолженность: 1579,00 руб.</p>  
           </div>
-
           
         </form>
 
       </div>
       
-
-
-
-
-      <!-- Create -->
-      <div class="mt-10 p-8 flex items-start bg-light-grey rounded-md shadow-lg">
-        
-        <!-- Form -->
-        <form @submit.prevent="createWorkout" class="flex flex-col gap-y-5 w-full">
-
-          <h1 class="text-2xl text-at-light-green">Record Workout</h1>
-
-          <!-- Workout Name -->
-          <div class="flex flex-col">
-            <label for="workout-name" class="mb-1 text-sm text-at-light-green">Workout Name</label>
-            <input 
-              type="text" 
-              required 
-              class="p-2 text-gray-500 focus:outline-none" 
-              id="workout-name"
-              v-model="workoutName"
-            >
-          </div>
-
-          <!-- Workout Type -->
-          <div class="flex flex-col">
-            <label for="workout-type" class="mb-1 text-sm text-at-light-green">Workout Type</label>
-            <select 
-              id="workout-type" 
-              class="p-2 text-gray-500 focus:outline-none" 
-              required
-              @change="workoutChange"
-              v-model="workoutType"
-            >
-              <option value="select-workout">Select Workout</option>
-              <option value="strength">Strength Training</option>
-              <option value="cardio">Cardio Training</option>
-            </select>
-          </div>
-
-          <!-- Srength Training Inputs -->
-          <div v-if="workoutType === 'strength'" class="flex flex-col gap-x-y-4"> 
-            <div 
-              class="flex flex-col gap-x-6 gap-y-2 relative md:flex-row" 
-              v-for="(item, index) in exercises"
-              :key="index"
-            >
-              <div class="flex flex-col md:w-1/3">
-                <label for="exercise-name" class="mb-1 text-sm text-at-light-green">Exercise</label>
-                <input 
-                  type="text" 
-                  required 
-                  class="p-2 w-full text-gray-500 focus:outeline-none"
-                  v-model="item.exercise"
-                >
-              </div>
-              <div class="flex flex-col flex-1">
-                <label for="sets" class="mb-1 text-sm text-at-light-green">Sets</label>
-                <input 
-                  type="text" 
-                  required 
-                  class="p-2 w-full text-gray-500 focus:outeline-none"
-                  v-model="item.sets"
-                >
-              </div>
-              <div class="flex flex-col flex-1">
-                <label for="reps" class="mb-1 text-sm text-at-light-green">Reps</label>
-                <input 
-                  type="text" 
-                  required 
-                  class="p-2 w-full text-gray-500 focus:outeline-none"
-                  v-model="item.reps"
-                >
-              </div>
-              <div class="flex flex-col flex-1">
-                <label for="weight" class="mb-1 text-sm text-at-light-green">Weight (LB's)</label>
-                <input 
-                  type="text" 
-                  required 
-                  class="p-2 w-full text-gray-500 focus:outeline-none"
-                  v-model="item.weight"
-                >
-              </div>
-              <img 
-                src="@/assets/images/trash-light-green.png" 
-                class="h-4 w-auto absolute -left-5 cursor-pointer" 
-                alt=""
-                @click="deleteExercise(item.id)"
-              >
-            </div>
-              <button 
-                @click="addExercise"
-                type="button" 
-                class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
-              >
-                Add Exercise
-              </button>
-          </div>
-
-          <!-- Cardio Inputs -->
-          <div v-if="workoutType === 'cardio'" class="flex flex-col gap-x-y-4"> 
-            <div 
-              class="flex flex-col gap-x-6 gap-y-2 relative md:flex-row" 
-              v-for="(item, index) in exercises"
-              :key="index"
-            >
-              <div class="flex flex-col md:w-1/3">
-                <label for="cardio-type" class="mb-1 text-sm text-at-light-green">Type</label>
-                <select 
-                  id="cardio-type" 
-                  class="p-2 w-full text-gray-500 focus:outline-none"
-                  v-model="item.cardioType"
-                >
-                  <option value="#">Select Type</option>
-                  <option value="run">Runs</option>
-                  <option value="walk">Walk</option>
-                </select>
-              </div>
-              <div class="flex flex-col flex-1">
-                <label for="distance" class="mb-1 text-sm text-at-light-green">Distance</label>
-                <input 
-                  type="text" 
-                  required 
-                  class="p-2 w-full text-gray-500 focus:outeline-none"
-                  v-model="item.distance"
-                >
-              </div>
-              <div class="flex flex-col flex-1">
-                <label for="duration" class="mb-1 text-sm text-at-light-green">Duration</label>
-                <input 
-                  type="text" 
-                  required 
-                  class="p-2 w-full text-gray-500 focus:outeline-none"
-                  v-model="item.duration"
-                >
-              </div>
-              <div class="flex flex-col flex-1">
-                <label for="pace" class="mb-1 text-sm text-at-light-green">Pace</label>
-                <input 
-                  type="text" 
-                  required 
-                  class="p-2 w-full text-gray-500 focus:outeline-none"
-                  v-model="item.pace"
-                >
-              </div>
-              <img 
-                @click="deleteExercise(item.id)"
-                src="@/assets/images/trash-light-green.png" 
-                class="h-4 w-auto absolute -left-5 cursor-pointer" 
-                alt=""
-              >
-            </div>
-              <button
-                @click="addExercise" 
-                type="button" 
-                class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
-              >
-                Add Exercise
-              </button>
-          </div>
-
-            <button 
-              type="submit" 
-              class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
-            >
-              Record Workout
-            </button>
-        </form>
-      </div>
     </main>
   </div>
   
@@ -1046,11 +876,17 @@ export default {
   .totalMenu_wrapper {
     height: 100vh;
     background-color: rgba(0, 0, 0, 0.7);
+    backdrop-filter: blur(2px);
   }
 
   .deal-details {
     height: 100%;
     overflow-y: scroll;
+  }
+
+  .deal_information_inputs {
+    position: fixed;
+    padding: 0 1rem;
   }
 
   .more-arrow {
