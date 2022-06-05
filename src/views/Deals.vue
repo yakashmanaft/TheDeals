21544b5cde5603f4f252f7f550a836f332d8d34e
Sergey <anfalov@camini-pr.ru>
2022-06-05 19:32:31 +0500

    добавлен список статусов для динамического отображения, подготвлены функции обновления статуса конкретного дела, начали задавать стили

--- src/views/Deals.vue
+++ src/views/Deals.vue
@@ -130,16 +130,18 @@
           <!-- menu header -->
           <div class="text-blue flex items-center place-content-between px-4 my-4">
             <span class="dealStatusMenu-btn_close">Отменить</span>
-            <span>Готово</span>
+            <span @click="updateStatus">Готово</span>
           </div>
           <!-- menu content -->
-          <div class="px-4 mt-2 mb-4">
-            <p>Бронь даты</p>
-            <p>В процессе</p>
-            <p>В доставке</p>
-            <p>Долг</p>
-            <p>Завершен</p>
-            <p>Отменен</p>
+          <div class="mb-4">
+            <!-- status item -->
+            <div v-for="(status, index) in dealStatusList" :key="index" class="flex flex-col mt-1 px-4">
+              <!--  -->
+              <input class="custom-radio" name="changeDealStatus" type="radio" :id="`${status.name}1`" :value="status.name"
+              v-model="statusDeal.currentDealStatus" >
+              <label :for="`${status.name}1`">{{ status.title }}</label>
+
+            </div>
           </div>
         </div>
       </div>
@@ -175,12 +177,21 @@ export default {
 
     // deal status menu
     const dealStatusMenu = ref(false);
+
+    // Помещаем сюда current статус выбранного дела
+    const statusDeal = ref('')
+
     // deal status menu toggle
+    // Забираем у текущего дела id и значение статуса 
     const dealStatusMenuToggle = (currentDealID, currentDealStatus) => {
       dealStatusMenu.value = !dealStatusMenu.value;
-      console.log(dealStatusMenu.value)
-      console.log(currentDealID)
-      console.log(currentDealStatus)
+      
+      const currentDeal = {
+        currentDealID: currentDealID,
+        currentDealStatus: currentDealStatus
+      }
+      // console.log(currentDeal)
+      return statusDeal.value = currentDeal
     }
 
     const closeDealStatusMenu = (e) => {
@@ -188,6 +199,19 @@ export default {
           dealStatusMenu.value = !dealStatusMenu.value;
       }
 
+    }
+
+    const updateStatus = () => {
+      // Закрываем меню изменения статуса
+      dealStatusMenu.value = !dealStatusMenu.value;
+
+      // Преобразования и вычисления
+      const deal = statusDeal.value
+      console.log(deal.currentDealID)
+      console.log(deal.currentDealStatus)
+
+      // Обновляем данные в БД
+      // Требуется функция проверки дела на долги!!!
     }
 
     //
@@ -339,7 +363,7 @@ export default {
     }
 
     return {
-      list, setDealStatus, dataLoaded, title, executionDatesArray, translateDealType, translateDealStatus, showEventDate, daysArray, contactInfo, getNameId, checkChangeStatus, dealStatusArray, getDealStatus, getStatusArrLength, dealStatusList, spinner, dealStatusMenu, dealStatusMenuToggle, closeDealStatusMenu
+      list, setDealStatus, dataLoaded, title, executionDatesArray, translateDealType, translateDealStatus, showEventDate, daysArray, contactInfo, getNameId, checkChangeStatus, dealStatusArray, getDealStatus, getStatusArrLength, dealStatusList, spinner, dealStatusMenu, dealStatusMenuToggle, closeDealStatusMenu, statusDeal, updateStatus
     };
   },
 };
@@ -546,5 +570,67 @@ export default {
     background-color: rgba(0, 0, 0, 0.7);
     backdrop-filter: blur(2px);
   }
+
+  /* для элемента input c type="radio" */
+  /* .custom-radio {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+  } */
+
+  /* для элемента label связанного с .custom-radio */
+  .custom-radio+label {
+    display: inline-flex;
+    align-items: center;
+    user-select: none;
+  }
+
+  /* создание в label псевдоэлемента  before со следующими стилями */
+  .custom-radio+label::after {
+    content: '';
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    flex-shrink: 0;
+    flex-grow: 0;
+    border: 1px solid #adb5bd;
+    border-radius: 50%;
+    margin-left: auto;
+    margin-right: 1%;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 50% 50%;
+  }
+
+  /* стили при наведении курсора на радио */
+  /* .custom-radio:not(:disabled):not(:checked)+label:hover::before {
+    border-color: #b3d7ff;
+  } */
+
+  /* стили для активной радиокнопки (при нажатии на неё) */
+  /* .custom-radio:not(:disabled):active+label::before {
+    background-color: #b3d7ff;
+    border-color: #b3d7ff;
+  } */
+
+  /* стили для радиокнопки, находящейся в фокусе */
+  /* .custom-radio:focus+label::before {
+    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+  } */
+
+  /* стили для радиокнопки, находящейся в фокусе и не находящейся в состоянии checked */
+  /* .custom-radio:focus:not(:checked)+label::before {
+    border-color: #80bdff;
+  } */
+  /* стили для радиокнопки, находящейся в состоянии checked */
+  .custom-radio:checked+label::after {
+    border-color: #0b76ef;
+    background-color: #0b76ef;
+    /* background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e"); */
+  }
+  /* стили для радиокнопки, находящейся в состоянии disabled */
+  /* .custom-radio:disabled+label::before {
+    background-color: #e9ecef;
+  } */
 
 </style>
