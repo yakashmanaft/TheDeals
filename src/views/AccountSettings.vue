9b9e9f084bed2b9d107290bbc1ec91c879e1def7
ya.kashmanaft <ya.kashmanaft@yandex.ru>
2022-06-14 23:25:17 +0500

    Подготовка для БД по настройкам аккаунта - рецепты

--- src/views/AccountSettings.vue
+++ src/views/AccountSettings.vue
@@ -8,6 +8,19 @@
       <li>- список дополнительных атрибутов (посуда, подставки, упаковка)</li>
       <li>- списки статусов по делам</li>
     </ul>
+    <div v-for="(item, index) in assortmentList" :key="index">
+
+      <span>{{ item.title }}</span>
+      <ul v-for="(recipe, idx) in item.recipes" :key="idx">
+        <li class="text-sm text-dark-gray">
+          <span>{{recipe.title}}</span>
+          <ul v-for="(step, n) in recipe.steps" :key="n">
+            <li>{{ n }}.{{ step }}</li>
+          </ul>
+        </li>
+      </ul>
+
+    </div>
   </div>
 </template>
 
@@ -28,9 +41,81 @@
       // Берем имя роута для заголовка
       const router = useRouter();
       const title = router.currentRoute._value.meta.translation;
+
+      //Рецепты
+      // Ассортимент по предметам заказа (устанавливается в настройках аккаунта)
+      const assortmentList = [
+        {
+          name: 'cake',
+          img: 'cake.png',
+          title: 'Торт',
+          recipes: [
+            {
+              title: 'Молочная девочка'
+            },
+            {
+              title: 'Красный бархат'
+            }
+          ]
+        },
+        {
+          name: 'wedding-cake',
+          img: 'wedding-cake.png',
+          title: 'Свадебный торт',
+        },
+        {
+          name: 'cupcake',
+          img: 'cupcake.png',
+          title: 'Капкейк',
+        },
+        {
+          name: 'meringue-roll',
+          img: 'meringue-roll.png',
+          title: 'Меренговый рулет',
+        },
+        {
+          name: 'brownies',
+          img: 'brownies.png',
+          title: 'Брауни',
+        },
+        {
+          name: 'meringue',
+          img: 'meringue.png',
+          title: 'Меренге (Безе)',
+          recipes: [
+            {
+              title: 'Швейцарская меренга (Безешка по-нашему)',
+              steps: [
+                'На 1 часть белком берем 2 части сахара',
+                'Соединяем все в термоустойчивой миске и ставим на водяную баню. Взбиваем белки до растворения сахара (прямо в водяной бане)',
+                'Затем снимаем с бани и продолжаем взбивать до остывания, доводя до состояния крепких пиков ("Птичий клюв")',
+                'После перекладываем массу в кондитерской мешок с нужной насадкой и',
+                'Отсаживаем в пергаменте',
+                'Ставим духовку на 85 - 90С сушиться на 2-3 часа',
+                'И все готово'
+              ]
+            },
+          ]
+        },
+        {
+          name: 'pavlova',
+          img: 'pavlova.png',
+          title: 'Павлова',
+        },
+        {
+          name: 'cake-pop',
+          img: 'cake-pop.png',
+          title: 'Кейк-попсы',
+        },
+        {
+          name: 'cake-eskimos',
+          img: 'cake-eskimos.png',
+          title: 'Эскимошки'
+        }
+      ]  
       
       return {
-        user, title
+        user, title, assortmentList
       }
     }
   }
