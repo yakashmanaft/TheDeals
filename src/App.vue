d046ad50e08743bc73c99ba7666b4222917bfee0
ya.kashmanaft <ya.kashmanaft@yandex.ru>
2022-07-03 16:18:16 +0500

    установлен ionic framework, попытки реализовать меню не отдельным экраном

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,68 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <ion-menu side="end" menu-id="first" content-id="main">
+      <ion-header>
+        <ion-toolbar color="primary">
+          <ion-title>Example Menu</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <ion-list>
+          <ion-item>Menu Item</ion-item>
+          <ion-item><router-link to="/">Home</router-link></ion-item>
+          <ion-item><router-link to="/about">About</router-link></ion-item>
+          <PWAPrompt/>
+        </ion-list>
+      </ion-content>
+    </ion-menu>
+
+    <div class="ion-page" id="main" type="push">
+      <ion-content>
+        <ion-menu-toggle>
+          <ion-button>Toggle Menu</ion-button>
+        </ion-menu-toggle>
+        <router-view/>
+      </ion-content>
+    </div>
+  </div>
 </template>
+
+<script>
+import PWAPrompt from "./components/PWAPrompt.vue";
+import { ref } from 'vue';
+import { defineComponent } from 'vue';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonItem, 
+  IonList, 
+  IonMenu, 
+  IonMenuToggle,
+  IonButton,
+  IonTitle, 
+  IonToolbar
+} from '@ionic/vue';
+
+export default defineComponent ({
+    components: {
+      PWAPrompt,
+    IonContent, 
+    IonHeader, 
+    IonItem, 
+    IonList, 
+    IonMenu, 
+    IonMenuToggle,
+    IonButton,
+    IonTitle, 
+    IonToolbar
+    },
+    setup() {
+      return {
+        
+      }
+    }
+})
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import { IonicVue } from '@ionic/vue';
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
 
-createApp(App).use(store).use(router).mount('#app')
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+// createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App).use(IonicVue).use(router);
+
+router.isReady().then(() => {
+  app.mount('#app');
+});
