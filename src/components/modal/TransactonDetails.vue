c4cbb52ed536a0240eb1f1d319e2ce0908f5e17e
Sergey <anfalov@camini-pr.ru>
2022-11-17 18:53:46 +0500

    доделали стили в transaction details

--- src/components/modal/TransactonDetails.vue
+++ src/components/modal/TransactonDetails.vue
@@ -1,8 +1,8 @@
 <template>
-    <ion-modal :initial-breakpoint="0.6">
+    <ion-modal :initial-breakpoint="0.5">
         <ion-header translucent="true">
             <ion-toolbar>
-                <ion-title>Детали</ion-title>
+                <!-- <ion-title>Детали</ion-title> -->
                 <ion-buttons slot="end">
                     <ion-button @click="$emit('closeModal')">Закрыть</ion-button>
                 </ion-buttons>
@@ -10,7 +10,7 @@
         </ion-header>
         <ion-content
             :scroll-events="true"
-            class="ion-page ion-margin-top" 
+            class="ion-page ion-margin-top ion-text-center" 
             id="main"
             type="push"
             forceOverscroll="false"
@@ -18,11 +18,28 @@
             <br>
             <br>
             <br>
-            {{currentTransaction}}
+            <ion-grid>
+                <ion-row class="ion-justify-content-center ion-padding-vertical">
+                    <ion-text>
+                        {{formattedDate(currentTransaction.created_at)}}
+                    </ion-text>
+                </ion-row>
 
-            <div @click="goToDeal(currentTransaction.dealID)">
-                Перейти к делу
-            </div>
+                <ion-row class="ion-justify-content-center ion-margin-vertical ion-padding-vertical">
+                    <ion-text style="font-size: 2rem; font-weight: bold;" :color="getAmountColor(currentTransaction.dealType)">
+                        <span v-if="currentTransaction.dealType === 'sale'">+</span>
+                        <span v-else>-</span>
+                        {{currentTransaction.amount.toFixed(2)}} {{currency}}
+                    </ion-text>
+                </ion-row>
+
+                <ion-row class="ion-justify-content-center ion-padding-top">
+                    <ion-button @click="goToDeal(currentTransaction.dealID)" color="dark" shape="round">
+                        Перейти к делу
+                    </ion-button>
+                </ion-row>
+            </ion-grid>
+            <!-- {{currentTransaction}} -->
         </ion-content>
     </ion-modal>
 </template>
@@ -31,9 +48,12 @@
     import { defineComponent, ref, onMounted, computed, watchEffect } from 'vue'; 
     import { useRouter } from 'vue-router';
     //
-    import { IonModal, IonHeader, IonContent, IonToolbar, IonTitle, IonButtons, IonButton } from '@ionic/vue';
+    import { IonModal, IonHeader, IonContent, IonToolbar, IonTitle, IonButtons, IonButton, IonText, IonGrid, IonRow, IonIcon } from '@ionic/vue';
     //
     import store from '../../store/index';
+    //
+    import { format, parseISO, formatISO  } from 'date-fns';
+    import { ru } from 'date-fns/locale';
 
     export default defineComponent({
         name: 'TransactionDetails',
@@ -41,9 +61,11 @@
             transaction: Object
         },
         components: {
-            IonModal, IonHeader, IonContent, IonToolbar, IonTitle, IonButtons, IonButton
+            IonModal, IonHeader, IonContent, IonToolbar, IonTitle, IonButtons, IonButton, IonText, IonGrid, IonRow, IonIcon
         },
         setup(props, { emit }) {
+            // Currency
+            const currency = ref(store.state.systemCurrency.name)
             // Setup ref to router
             const router = useRouter();
             //
@@ -67,12 +89,28 @@
                 })
             }
 
+            // функция форматирования даты для сравнения даты дела и даты дня
+            const formattedDate = (day) => {
+                const formattedString = format(parseISO(day), 'd MMMM Y, HH:mm', { locale: ru });
+
+                return formattedString;
+            }
+
+            //
+            const getAmountColor = (transactionDealType) => {
+                if(transactionDealType === 'sale') {
+                    return 'success'
+                } else {
+                    return 'medium'
+                }
+            }
+
             watchEffect(() => {
                 currentTransaction.value = props.transaction
             })
 
             return {
-                currentTransaction, myDeals, goToDeal, router
+                currentTransaction, myDeals, goToDeal, router, formattedDate, currency, getAmountColor
             }
         }
     })
