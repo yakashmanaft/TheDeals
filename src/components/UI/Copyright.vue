1dac681267c5198735c91d701d1f1f3213255d13
yakashmanaft <ya.kashmanaft@yandex.ru>
2023-09-13 00:09:48 +0500

    исправален номер в компоненте copyright

--- src/components/UI/Copyright.vue
+++ src/components/UI/Copyright.vue
@@ -2,7 +2,7 @@
     <footer 
         style="position: fixed; bottom: 0; width: 100%; margin-bottom: 2rem;"
     >
-        <a href='https://t.me/+79223000705'>
+        <a href='https://t.me/+79617582573'>
             <!-- <a href='https://t.me/+79526406146'> -->
 
             @Sergey Anfalov, 2023
